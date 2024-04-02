cfd8856b863acfc4df99c9a435d23fcf01319d3a
nirvananiroomand <nirvana.niroomand@gmail.com>
2024-04-02 14:24:38 +0430

    add script for generating questions using OpenAI API

---
+++ ai_engine/open_ai.py
@@ -0,0 +1,24 @@
+import os
+from openai import OpenAI
+from dotenv import load_dotenv
+
+
+def generate_questions(content, difficulty, types_of_questions):
+    client = OpenAI(api_key=os.getenv("OPENAI_API_KEY2"))
+
+    completion = client.chat.completions.create(
+        model="gpt-4",
+        messages=[
+            {"role": "system",
+             "content": "You are an expert teaching assistant who can make great questions out of a given context."},
+            {"role": "user", "content": f"""please provide {types_of_questions} {difficulty} question(s) from this text:{content}"""}  
+        ]
+    )
+
+    return completion.choices[0].message.content
+
+
+if __name__ == '__main__':
+    load_dotenv()
+    generate_questions(content, quantity, difficulty, types_of_questions)
+
