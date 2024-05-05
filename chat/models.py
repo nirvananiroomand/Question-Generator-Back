511770061e213467306de404fd6e35fda5140d1a
nirvananiroomand <nirvana.niroomand@gmail.com>
2024-05-05 16:34:25 +0330

    add title field to chat model

--- chat/models.py
+++ chat/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Chat(models.Model):
+    title = models.CharField(max_length=100)
     content = models.TextField()
     difficulty = models.CharField(max_length=30)
     created_at = models.DateTimeField(auto_now_add=True)
