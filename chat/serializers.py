b5d9701ff050788928b71a0305a7613051e939c5
nirvananiroomand <nirvana.niroomand@gmail.com>
2024-05-05 16:34:49 +0330

    update chat api

--- chat/serializers.py
+++ chat/serializers.py
@@ -2,7 +2,13 @@ from rest_framework import serializers
 from chat.models import Chat
 
 
-class ChatSerializer(serializers.ModelSerializer):
+class ChatTitleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Chat
+        fields = ('id', 'title', 'created_at')
+
+
+class ChatCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Chat
         fields = '__all__'
--- chat/views.py
+++ chat/views.py
@@ -4,7 +4,7 @@ from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework.request import Request
 
-from chat.serializers import ChatSerializer
+from chat.serializers import ChatTitleSerializer, ChatCreateSerializer
 from chat.models import Chat
 
 
@@ -13,12 +13,12 @@ def get_all_chats_api(request: Request):
 
     if request.method == 'GET':
         query_set = Chat.objects.all()
-        ser = ChatSerializer(query_set, many=True)
+        ser = ChatTitleSerializer(query_set, many=True)
 
         return Response({'chats': ser.data}, status=status.HTTP_200_OK)
 
     elif request.method == 'POST':
-        ser = ChatSerializer(data=request.data)
+        ser = ChatCreateSerializer(data=request.data)
         if ser.is_valid():
             ser.save()
             return Response(ser.data, status=status.HTTP_201_CREATED)
