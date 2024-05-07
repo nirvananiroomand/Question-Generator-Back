8baa09aeaf6b4a1d6f9634d78acbe99083be0a64
nirvananiroomand <nirvana.niroomand@gmail.com>
2024-05-07 14:12:28 +0330

    create ChatQuestionSerializer and ChatDetailSerializer

--- chat/serializers.py
+++ chat/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from chat.models import Chat
+from chat.models import Chat, ChatQuestion
 
 
 class ChatTitleSerializer(serializers.ModelSerializer):
@@ -9,6 +9,18 @@ class ChatTitleSerializer(serializers.ModelSerializer):
 
 
 class ChatCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Chat
+        fields = '__all__'
+
+
+class ChatQuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ChatQuestion
+        fields = '__all__'
+
+
+class ChatDetailsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Chat
         fields = '__all__'
