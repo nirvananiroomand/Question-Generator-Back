10c96e205ca5cbc16c8ce5938493e117b3a96b3e
nirvananiroomand <nirvana.niroomand@gmail.com>
2024-05-21 17:16:28 +0330

    create custom user admin class

--- user/admin.py
+++ user/admin.py
@@ -2,5 +2,28 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
+
 # Register your models here.
-admin.site.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        ("Personal info", {"fields": ("email", )}),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+    list_display = ("username", "email", "is_staff")
+    search_fields = ("username", "email")
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
